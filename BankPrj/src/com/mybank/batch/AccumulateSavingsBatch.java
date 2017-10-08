e7dd351a98358c534d50353de8510eceb9f6795f
Yurik16 <chuksin.yury@gmail.com>
2017-10-08 20:13:17 +0200

    lab 6-28 Adding Batch Program

---
+++ BankPrj/src/com/mybank/batch/AccumulateSavingsBatch.java
@@ -0,0 +1,37 @@
+package com.mybank.batch;
+
+import com.mybank.domain.Account;
+import com.mybank.domain.Bank;
+import com.mybank.domain.Customer;
+import com.mybank.domain.SavingsAccount;
+
+/**
+ * 
+ * class AccumulateSavingsBatch.
+ * 
+ * @author Yury Chuksin
+ * @version 1.0.0
+ * @since 06-10-2017
+ */
+public class AccumulateSavingsBatch {
+	
+	private Bank bank;
+	
+	public void doBatch() {
+
+		for(int i = 0; i < bank.getNumOfCustomers(); i++) {
+			Customer cust = bank.getCustomer(i);
+			for(int j = 0; j < cust.getNumOfAccounts(); j++) {
+				Account acc = cust.getAccount(j);
+				if(acc instanceof SavingsAccount) {
+					SavingsAccount savingAcc = (SavingsAccount) acc;
+					savingAcc.accumulateInterest();
+				}
+			}
+		}
+	}
+	
+	public void setBank(Bank b) {
+		this.bank = b;
+	}
+}
